3044623620dbb8f4aea19044dd8d3222ac28716a
Aftab Ahmed <aftab.ahmed@newfold.com>
2023-08-28 19:59:27 +0530

    front end changes2

--- frontend/src/pages/Share.js
+++ frontend/src/pages/Share.js
@@ -1,21 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import {useParams} from 'react-router-dom'
 
 function Share() {
     const [event, setEvent] = useState();
+    const {username , eventId} = useParams()
+    console.log(`${username} -- ${eventId}`)
     async function getData() {
         try {
             let res = await axios({
                 url: 'http://localhost:3069/getPost',
                 method: 'GET',
+                params :{
+                    "username" : username,
+                    "postId" : eventId
+                },
                 timeout: 8000,
                 headers: {
                     'Content-Type': 'application/json'
                 }
             })
 
-            console.log(res.data.data)
-            return res.data.data
+            console.log(res.data)
+            return res.data
         } catch (error) {
             console.error(error);
         }
