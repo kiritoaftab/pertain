160fc74a2dc6f75cf7da66a8b0eae773ca7fef56
Vipul-J <jujarvipul21@gmail.com>
2023-09-07 01:46:45 +0530

    search bar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,6 +12,7 @@ import logo from "../assets/images/logo.png"
 const Home = () => {
   const [madarchodState, setMadarchodState] = useState();
   const [events, setEvents] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   async function getData() {
     try {
@@ -43,6 +44,13 @@ const Home = () => {
       <Nav />
       <section>
         <div className='container px-5'>
+          <input
+            type="text"
+            placeholder="Search by username..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+
           {
             Array.isArray(madarchodState) ?
               madarchodState.map((events, index) => {
@@ -53,78 +61,80 @@ const Home = () => {
                 if (Object.keys(useEvents).length === 0) {
 
                 } else {
-                  return (
-                    <div key={index}>
-                      <h1>     </h1>
-                      {/* {JSON.stringify(useEvents)} */}
-                      {
-                        Object.keys(useEvents).map((key) => {
-                          const shareUrl = `/${userKey}/${key}`
-                          return (
-                            <div key={key}>
-                              <div className='row'>
-                                <div className='col-lg-12 d-flex justify-content-center align-items-center'>
-                                  <div className=" card border rounded shadow-sm mt-3 " style={{ maxWidth: '700px' }}>
-                                    <div className="d-flex align-items-center">
-                                      <img src={logo} alt="profileImage" className="rounded-circle mx-4" height={60} />
-                                      <div>
-                                        <h5 className="fw-bold mt-0 mb-0 text-info" style={{ fontSize: '18px' }}>{userKey}</h5>
-                                        <p className="fw-bold mt-0 mb-0 text-muted" style={{ fontSize: '16px' }}>Organised By :</p>
-                                        <div className='row'>
-                                          <div className="col-lg-8">
-                                            <h2 className="fw-bold mt-1 mb-0" style={{ fontSize: '18px' }}>   {useEvents[key].organizerName ? useEvents[key].organizerName : ""}</h2>
+                  if (useEvents && (!searchTerm || (useEvents.organizerName && useEvents.organizerName.includes(searchTerm)))) {
+                    return (
+                      <div key={index}>
+                        <h1>     </h1>
+                        {/* {JSON.stringify(useEvents)} */}
+                        {
+                          Object.keys(useEvents).map((key) => {
+                            const shareUrl = `/${userKey}/${key}`
+                            return (
+                              <div key={key}>
+                                <div className='row'>
+                                  <div className='col-lg-12 d-flex justify-content-center align-items-center'>
+                                    <div className=" card border rounded shadow-sm mt-3 " style={{ maxWidth: '700px' }}>
+                                      <div className="d-flex align-items-center">
+                                        <img src={logo} alt="profileImage" className="rounded-circle mx-4" height={60} />
+                                        <div>
+                                          <h5 className="fw-bold mt-0 mb-0 text-info" style={{ fontSize: '18px' }}>{userKey}</h5>
+                                          <p className="fw-bold mt-0 mb-0 text-muted" style={{ fontSize: '16px' }}>Organised By :</p>
+                                          <div className='row'>
+                                            <div className="col-lg-8">
+                                              <h2 className="fw-bold mt-1 mb-0" style={{ fontSize: '18px' }}>   {useEvents[key].organizerName ? useEvents[key].organizerName : ""}</h2>
+                                            </div>
+                                            <div className="col-lg-4">
+                                              <p className="fw-bold mt-1 mb-0 badge bg-info text-white" style={{ fontSize: '14px' }}>{useEvents[key].genre ? useEvents[key].genre : ""}</p>
+                                            </div>
                                           </div>
-                                          <div className="col-lg-4">
-                                            <p className="fw-bold mt-1 mb-0 badge bg-info text-white" style={{ fontSize: '14px' }}>{useEvents[key].genre ? useEvents[key].genre : ""}</p>
+                                        </div>
+                                      </div>
+
+                                      <div className="event-banner mt-4 mb-4">
+                                        <img src={useEvents[key].imgUrl[0]} alt="event-banner" className="rounded img-fluid" />
+                                        <div className='row mt-4'>
+                                          <div className="col-lg-8 d-flex justify-content-center align-items-center">
+                                            <h2 className="fw-bolder">
+                                              {useEvents[key].eventName ? useEvents[key].eventName : ""}
+                                            </h2>
+                                          </div>
+                                          <div className="col-lg-4 d-flex justify-content-center align-items-center">
+                                            <p className='fs-5'><span className='text-info fw-bolder fs-4 me-2'>&#x20B9;</span>  {useEvents[key].price ? useEvents[key].price : ``} <a href={shareUrl}> <FontAwesomeIcon icon={faShare} size="lg" className="me-1 text-info" /></a></p>
+
                                           </div>
                                         </div>
-                                      </div>
-                                    </div>
 
-                                    <div className="event-banner mt-4 mb-4">
-                                      <img src={useEvents[key].imgUrl[0]} alt="event-banner" className="rounded img-fluid" />
-                                      <div className='row mt-4'>
-                                        <div className="col-lg-8 d-flex justify-content-center align-items-center">
-                                          <h2 className="fw-bolder">
-                                            {useEvents[key].eventName ? useEvents[key].eventName : ""}
-                                          </h2>
+                                        <h5 className="fs-6">
+                                          {useEvents[key].desc ? useEvents[key].desc : ""}
+                                        </h5>
+                                      </div>
+
+                                      <div className='row'>
+                                        <div className="col-lg-4 d-flex justify-content-center ">
+                                          <p className='fs-6'> <FontAwesomeIcon icon={faCalendar} size="lg" className="me-1 text-info" />{useEvents[key].date ? useEvents[key].date : ""}</p>
                                         </div>
-                                        <div className="col-lg-4 d-flex justify-content-center align-items-center">
-                                          <p className='fs-5'><span className='text-info fw-bolder fs-4 me-2'>&#x20B9;</span>  {useEvents[key].price ? useEvents[key].price : ``} <a href={shareUrl}> <FontAwesomeIcon icon={faShare} size="lg" className="me-1 text-info"/></a></p>
-                                        
+                                        <div className="col-lg-3 d-flex justify-content-center ">
+                                          <p className='fs-6'> <FontAwesomeIcon icon={faClock} size="lg" className="me-1 text-info" /> {useEvents[key].time ? useEvents[key].time : ""}</p>
                                         </div>
-                                      </div>
-
-                                      <h5 className="fs-6">
-                                        {useEvents[key].desc ? useEvents[key].desc : ""}
-                                      </h5>
-                                    </div>
-
-                                    <div className='row'>
-                                      <div className="col-lg-4 d-flex justify-content-center ">
-                                        <p className='fs-6'> <FontAwesomeIcon icon={faCalendar} size="lg" className="me-1 text-info" />{useEvents[key].date ? useEvents[key].date : ""}</p>
-                                      </div>
-                                      <div className="col-lg-3 d-flex justify-content-center ">
-                                        <p className='fs-6'> <FontAwesomeIcon icon={faClock} size="lg" className="me-1 text-info" /> {useEvents[key].time ? useEvents[key].time : ""}</p>
-                                      </div>
-                                      <div className="col-lg-5 d-flex justify-content-center ">
-                                        <p className='fs-6'>  <FontAwesomeIcon icon={faMapLocation} size="lg" className="me-1 text-info" />  {useEvents[key].location ? useEvents[key].location : ""}</p>
+                                        <div className="col-lg-5 d-flex justify-content-center ">
+                                          <p className='fs-6'>  <FontAwesomeIcon icon={faMapLocation} size="lg" className="me-1 text-info" />  {useEvents[key].location ? useEvents[key].location : ""}</p>
+                                        </div>
                                       </div>
                                     </div>
                                   </div>
                                 </div>
                               </div>
-                            </div>
 
 
-                          )
+                            )
 
-                        })
-                      }
-                    </div>
-                  )
+                          })
+                        }
+                      </div>
+                    )
+                  }
+                  
                 }
-
               }
               ) :
               "loading"
